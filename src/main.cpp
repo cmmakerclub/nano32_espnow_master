53a871df07c8b075865336898d2e1eccd2c743d7
Nat <nat@cmmc.io>
2018-08-29 13:30:28 +0700

    update led.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <Arduino.h> 
+#include <Arduino.h>
 #include <esp_now.h>
 #include <WiFi.h>
 #include <CMMC_NB_IoT.h>
@@ -7,29 +7,35 @@
 HardwareSerial Serial1(2);
 CMMC_PACKET_T pArr[60];
 int pArrIdx = 0;
-char espnowMsg[300]; 
+char espnowMsg[300];
 
 #define rxPin (16)
 #define txPin (17)
 
+#define BOARD_LED (25)
 #define GREEN_LED (23)
 #define RED_LED (22)
+
 #define RESET_PIN (13)
 
-#define LED_PIN 
+#define LED_PIN
 
 CMMC_NB_IoT nb(&Serial1);
-uint32_t counter = 0; 
-uint32_t sentCnt = 0; 
+uint32_t counter = 0;
+uint32_t sentCnt = 0;
 
 void str2Hex(const char* text, char* buffer);
 void toHexString(const uint8_t array[], size_t len, char buffer[]);
 
-uint8_t currentSleepTimeMinuteByte = 15; 
+uint8_t currentSleepTimeMinuteByte = 5;
+uint32_t msAfterESPNowRecv = millis();
+
 bool dirty = false;
 bool isNbConnected = false;
+
 // String token = "b98ce7b0-185b-11e8-8630-b33e669e2295";
-String token = "f169c8e0-a872-11e8-8e2c-19a3b7904cb9";
+// String token = "f169c8e0-a872-11e8-8e2c-19a3b7904cb9";
+String token = "3ffbfb30-aaae-11e8-8e2c-19a3b7904cb9";
 char tokenHex[100];
 uint32_t prev;
 // uint8_t remoteMac[6] = {0x2e, 0x3a, 0xe8, 0x12, 0xbe, 0x92};
@@ -47,17 +53,15 @@ void setup() {
   delay(100);
   digitalWrite(RESET_PIN, LOW);
   delay(100); 
+  ledcAttachPin(BOARD_LED, 1);
 
+  ledcSetup(1, 12000, 8);
+  ledcWrite(1, 20);
   pinMode(GREEN_LED, OUTPUT);
-  pinMode(RED_LED, OUTPUT); 
-  pinMode(25, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
 
-  digitalWrite(GREEN_LED, HIGH); 
-  digitalWrite(RED_LED, HIGH); 
-  // digitalWrite(23, HIGH); 
-  // digitalWrite(25, HIGH); 
-  delay(500);
-
+  digitalWrite(GREEN_LED, HIGH);
+  digitalWrite(RED_LED, HIGH);
 
 
   Serial.println("Serial1..");
@@ -70,6 +74,7 @@ void setup() {
   Serial.println("[2]..");
   Serial.print("STA MAC: "); Serial.println(WiFi.macAddress());
   Serial.print("AP MAC: "); Serial.println(WiFi.softAPmacAddress());
+
   str2Hex(token.c_str(), tokenHex);
   Serial.println(tokenHex);
   if (esp_now_init() == ESP_OK) {
@@ -79,10 +84,11 @@ void setup() {
     Serial.println("ESPNow Init Failed");
     ESP.restart();
   }
-    delay(100);
-    nb.setDebugStream(&Serial);
+  delay(100);
+  nb.setDebugStream(&Serial);
 
   nb.onDeviceReboot([]() {
+    ledcWrite(1, 0);
     Serial.println(F("[user] Device rebooted."));
     digitalWrite(GREEN_LED, LOW);
     digitalWrite(RED_LED, LOW);
@@ -107,33 +113,40 @@ void setup() {
   });
 
   nb.onConnecting([]() {
+    ledcWrite(1, 0);
+    delay(300);
+    ledcWrite(1, 20);
+    Serial.println(F("[user] Device rebooted."));
     Serial.println("Connecting to NB-IoT...");
     digitalWrite(GREEN_LED, !digitalRead(GREEN_LED));
-    delay(500);
+    delay(300);
   });
 
   nb.onConnected([]() {
+    ledcWrite(1, 20);
+    Serial.println(F("[user] Device rebooted."));
     digitalWrite(RED_LED, HIGH);
     digitalWrite(GREEN_LED, HIGH);
+    ledcWrite(1, 20);
     Serial.print("[user] NB-IoT Network connected at (");
     Serial.print(millis());
     Serial.println("ms)");
     delay(3000);
-    Serial.println(nb.createUdpSocket("103.20.205.85", 5683, UDPConfig::DISABLE_RECV));
-    Serial.println(nb.createUdpSocket("103.212.181.167", 55566, UDPConfig::DISABLE_RECV));
+    Serial.println(nb.createUdpSocket("103.20.205.85", 5683, UDPConfig::ENABLE_RECV));
+    Serial.println(nb.createUdpSocket("103.212.181.167", 55566, UDPConfig::ENABLE_RECV));
     isNbConnected = 1;
     delay(1000);
   });
 
   Serial.println("WAIT... 2s");
   delay(2000);
-  nb.rebootModule();
-
+  nb.rebootModule(); 
   esp_now_register_send_cb([&] (const uint8_t *mac_addr, esp_now_send_status_t status) {
     sentCnt++;
   });
 
   esp_now_register_recv_cb([&](const uint8_t *mac_addr, const uint8_t *data, int data_len) {
+    digitalWrite(GREEN_LED, !digitalRead(GREEN_LED));
     memcpy(&slave.peer_addr, mac_addr, 6);
     CMMC_PACKET_T wrapped;
     CMMC_SENSOR_DATA_T packet;
@@ -150,15 +163,17 @@ void setup() {
 
     esp_now_send(mac_addr, &currentSleepTimeMinuteByte, 1);
     Serial.printf("(uint32_t) sleepTime = %lu\r\n", (uint32_t) currentSleepTimeMinuteByte);
-    for(int i =0 ; i < sizeof(wrapped); i++) {
-      Serial.printf("%02x", ((uint8_t*)&wrapped)[i]); 
+    for (int i = 0 ; i < sizeof(wrapped); i++) {
+      Serial.printf("%02x", ((uint8_t*)&wrapped)[i]);
     }
-    Serial.println(); 
+    Serial.println();
 
     esp_err_t addStatus = esp_now_add_peer(&slave);
     uint8_t time = 1;
     // esp_err_t result = esp_now_send(mac_addr, &time, 1);
     counter++;
+
+    msAfterESPNowRecv = millis();
   });
 
   prev = millis();
@@ -169,51 +184,78 @@ uint32_t lastSentOkMillis;
 unsigned int ct = 1;
 
 void loop() {
-  if ( (millis() - lastSentOkMillis) > 10800*1000) { 
-      ESP.deepSleep(1e6); 
+  if ( (millis() - lastSentOkMillis) > 10800 * 1000) {
+    ESP.deepSleep(1e6);
   }
   nb.loop();
-      if ( (pArrIdx > 0) && (isNbConnected)) {
-      char buffer[500];
-      char b[500];
-      Serial.printf("pArrIdx = %d\r\n", pArrIdx);
-      for (int i = pArrIdx - 1; i >= 0; i--) {
-        Serial.printf("reading idx = %d\r\n", i);
-        toHexString((uint8_t*)  &pArr[i], sizeof(CMMC_PACKET_T), (char*)espnowMsg);
-        sprintf(b, "{\"ct\":\"%lu\", \"payload\": \"%s\"}", ct++, espnowMsg);
-        str2Hex(b, buffer);
-        String p3 = "";
-        p3 += String("40");
-        p3 += String("020579");
-        p3 += String("b5");
-        p3 += String("4e42496f54"); // NB-IoT
-        p3 += String("0d");
-        p3 += String("17");
-        p3 +=  String(tokenHex);
-        p3 += String("ff");
-        p3 += String(buffer);
-        int rt = 0;
-        while (true) {
-          if (nb.sendMessageHex(p3.c_str(), 0)) {
-            Serial.println(">> [ais] socket0: send ok.");
-            pArrIdx--;
-            lastSentOkMillis = millis();
+  if ( (millis() - msAfterESPNowRecv) > 500 && (pArrIdx > 0) && (isNbConnected)) {
+    static char buffer[600];
+    bzero(buffer, sizeof(buffer));
+    static char b[300];
+    static char msgId[5];
+    Serial.printf("pArrIdx = %d\r\n", pArrIdx);
+    Serial.println();
+    Serial.println(String(ct, HEX));;
+    for (int i = pArrIdx - 1; i >= 0; i--) {
+      digitalWrite(RED_LED, !digitalRead(RED_LED));
+      Serial.printf("reading idx = %d\r\n", i);
+      toHexString((uint8_t*)  &pArr[i], sizeof(CMMC_PACKET_T), (char*)espnowMsg);
+      sprintf(b, "{\"ct\":\"%lu\", \"sleep\":\"%lu\", \"payload\": \"%s\"}", ct++, currentSleepTimeMinuteByte, espnowMsg);
+      str2Hex(b, buffer);
+      sprintf(msgId, "%04lu", ct);
+      Serial.printf("msgId = %s\r\n", msgId);
+      String p3 = "";
+      p3 += String("40");
+      p3 += String("02");
+      // p3 += String(ct, HEX);
+      p3 += String(msgId);
+      p3 += String("b5");
+      p3 += String("4e42496f54"); // NB-IoT
+      p3 += String("0d");
+      p3 += String("17");
+      p3 +=  String(tokenHex);
+      p3 += String("ff");
+      p3 += String(buffer);
+      Serial.println(p3);
+      int rt = 0;
+      while (true) {
+        ledcWrite(1, 20);
+        if (nb.sendMessageHex(p3.c_str(), 0)) { 
+          delay(300); 
+          ledcWrite(1, 0);
+
+          ledcWrite(1, 20);
+          nb.sendMessageHex(p3.c_str(), 0);
+          delay(300);
+          ledcWrite(1, 0);
+
+          ledcWrite(1, 20);
+          nb.sendMessageHex(p3.c_str(), 0);
+          delay(300);
+          ledcWrite(1, 0);
+
+          Serial.println(">> [ais] socket0: send ok.");
+          pArrIdx--;
+          lastSentOkMillis = millis();
+          delay(1000);
+          break;
+        }
+        else {
+          Serial.println(">> [ais] socket0: send failed.");
+          if (++rt > 5) {
+            delay(100);
+            // ESP.restart();
+            ESP.deepSleep(1e6);
+            delay(100);
             break;
           }
-          else {
-            Serial.println(">> [ais] socket0: send failed.");
-            if (++rt > 5) {
-              delay(100);
-              // ESP.restart();
-              ESP.deepSleep(1e6);
-              delay(100);
-              break;
-            }
-          }
         }
+        delay(100);
       }
-      prev = millis();
     }
+    digitalWrite(RED_LED, LOW);
+    prev = millis();
+  }
   // if (millis() - prev > 1000) {
   //   prev = millis();
   //   Serial.printf("rate: recv=%lu, sent=%lu hz\r", counter, sentCnt);
